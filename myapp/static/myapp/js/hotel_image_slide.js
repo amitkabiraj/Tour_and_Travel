c9127c12736b9209c418c762403ee9f60fcb3325
gopal <gopal.deshra@gmail.com>
2023-09-26 01:03:54 +0530

    js file

--- myapp/static/myapp/js/hotel_image_slide.js
+++ myapp/static/myapp/js/hotel_image_slide.js
@@ -1,32 +1,32 @@
 const slider = document.querySelector('.image');
-    const images = document.querySelectorAll('.hotel_hotel-image');
-    const prevBtn = document.querySelector('.prev-btn');
-    const nextBtn = document.querySelector('.next-btn');
+const images = document.querySelectorAll('.hotel_hotel-image');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
 
-    let currentIndex = 0;
+let currentIndex = 0;
 
-    function showImage(index) {
-        images.forEach((image, i) => {
-            if (i === index) {
-                image.style.transform = 'translateX(0)';
-            } else {
-                image.style.transform = 'translateX(100%)';
-            }
-        });
-    }
+function showImage(index) {
+    images.forEach((image, i) => {
+        if (i === index) {
+            image.style.transform = 'translateX(0)';
+        } else {
+            image.style.transform = 'translateX(100%)';
+        }
+    });
+}
 
-    function showNextImage() {
-        currentIndex = (currentIndex + 1) % images.length;
-        showImage(currentIndex);
-    }
-
-    function showPrevImage() {
-        currentIndex = (currentIndex - 1 + images.length) % images.length;
-        showImage(currentIndex);
-    }
-
-    prevBtn.addEventListener('click', showPrevImage);
-    nextBtn.addEventListener('click', showNextImage);
-
-    // Initially, show the first image
+function showNextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
     showImage(currentIndex);
+}
+
+function showPrevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
+}
+
+prevBtn.addEventListener('click', showPrevImage);
+nextBtn.addEventListener('click', showNextImage);
+
+// Initially, show the first image
+showImage(currentIndex);
