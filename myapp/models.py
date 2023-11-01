c493321dd4651a0836cbf5853ee53fdf7433b580
amitkabiraj <amitkabiraj555@gmail.com>
2023-11-01 11:39:13 +0530

    review

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
 # Create your models here.
+# class Review(models.Model):
 class Review(models.Model):
-    
+    image = models.URLField()
+    name = models.CharField(max_length=100)
+    review = models.TextField()
+    stars = models.IntegerField(choices=[(i, f"{i} star") for i in range(1, 6)])
+
+    def __str__(self):
+        return f"Review by {self.name}"
