f6984537c415d25f98ba1be64cb40c8d8fb1a48f
amitkabiraj <amitkabiraj555@gmail.com>
2023-11-02 01:32:31 +0530

    register

--- myapp/views.py
+++ myapp/views.py
@@ -1,59 +1,134 @@
-from django.shortcuts import render,HttpResponse
-from myapp.models import Hotel,Review
+from django.shortcuts import render, HttpResponse
+from myapp.models import Hotel, Review, ContactMessage, User
+
+
 # Create your views here.
 def index(request):
-    data=Review.objects.all()
-    return render(request, 'myapp/index.html',{'review_data':data})
+    data = Review.objects.all()
+    return render(request, "myapp/index.html", {"review_data": data})
     # return HttpResponse("this is homepage")
+
+
 def car(request):
-    return render(request, 'myapp/car/car.html') 
+    return render(request, "myapp/car/car.html")
+
+
 def tickets(request):
-    return render(request, 'myapp/others/tickets.html')
+    return render(request, "myapp/others/tickets.html")
+
+
 def tourplan(request):
-    return render(request, 'myapp/others/tour_plan.html') 
+    return render(request, "myapp/others/tour_plan.html")
+
+
 def restrurent(request):
-    return render(request, 'myapp/info/restrurent.html') 
-def hospital(request):    
-    return render(request, 'myapp/info/hospital.html')
-def medicine(request):    
-    return render(request, 'myapp/info/medicine.html')
-def atm(request):    
-    return render(request, 'myapp/info/atm.html')
-def pump(request):    
-    return render(request, 'myapp/info/pump.html')
+    return render(request, "myapp/info/restrurent.html")
+
+
+def hospital(request):
+    return render(request, "myapp/info/hospital.html")
+
+
+def medicine(request):
+    return render(request, "myapp/info/medicine.html")
+
+
+def atm(request):
+    return render(request, "myapp/info/atm.html")
+
+
+def pump(request):
+    return render(request, "myapp/info/pump.html")
+
+
 def places(request):
-    return render(request, 'myapp/place/places.html')
+    return render(request, "myapp/place/places.html")
+
+
 def terms(request):
-    return render(request, 'myapp/others/terms.html') 
+    return render(request, "myapp/others/terms.html")
+
+
 def privasy(request):
-    return render(request, 'myapp/others/privasy.html')
+    return render(request, "myapp/others/privasy.html")
+
+
 def review(request):
-    return render(request, 'myapp/others/review.html') 
+    return render(request, "myapp/others/review.html")
+
+
 def murshidabad(request):
-    return render(request, 'myapp/place/murshidabad.html') 
+    return render(request, "myapp/place/murshidabad.html")
+
+
 def nadia(request):
-    return render(request, 'myapp/place/nadia.html') 
+    return render(request, "myapp/place/nadia.html")
+
+
 def contact(request):
-    return render(request, 'myapp/contact.html') 
+    if request.method == "POST":
+        name = request.POST["name"]
+        email = request.POST["email"]
+        message = request.POST["message"]
+        
+        ContactMessage.objects.create(name=name, email=email, message=message)
+
+    return render(request, "myapp/contact.html")
+
+
 def about(request):
-    return render(request, 'myapp/about.html') 
+    return render(request, "myapp/about.html")
+
+
 # def admin_login(request):
 #     return render(request, 'myapp/admin/admin_login.html')
 def admin_panel(request):
-    return render(request, 'myapp/admin/adminpanel.html') 
+    return render(request, "myapp/admin/adminpanel.html")
+
+
 def user_login(request):
-    return render(request, 'myapp/user/user_login.html')
+    return render(request, "myapp/user/user_login.html")
+
+
 def user_panel(request):
-    return render(request, 'myapp/user/userpanel.html')
+    return render(request, "myapp/user/userpanel.html")
+
+
 def register(request):
-    return render(request, 'myapp/user/register.html')
+    if request.method == "POST":
+        name = request.POST["name"]
+        email = request.POST["email"]
+        phone =request.POST["phone"]
+        password= request.POST["password"]
+        picture = request.FILES.get("picture")
+        gender = request.POST["gender"]
+        country = request.POST["country"]
+
+
+    
+        User.objects.create(name=name, email=email, phone=phone, password=password,picture=picture,gender=gender, country=country)
+        # return render(request, "myapp/success.html",{"user": User})
+    
+    return render(request, "myapp/user/register.html")
+
+
 def all_images(request):
-    return render(request, 'myapp/hotel/all_images.html')
+    return render(request, "myapp/hotel/all_images.html")
+
+
 def hotel_booking(request):
-    return render(request, 'myapp/hotel/booking page.html')
+    return render(request, "myapp/hotel/booking page.html")
+
+
 def hotel(request):
-    data=Hotel.objects.all()
+    data = Hotel.objects.all()
     print("data comes")
-    return render(request,'myapp/hotel/hotel.html',{'hotel_data':data},)
+    return render(
+        request,
+        "myapp/hotel/hotel.html",
+        {"hotel_data": data},
+    )
+
+
 # def hotel(request):
 #     return render(request, 'myapp/hotel/hotel.html')
